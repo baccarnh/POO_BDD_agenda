27f6b6f65811e3fda4529d6f9ec3df02ce062c3f
krid <baccarnh@gmail.com>
2020-02-10 11:59:09 +0100

    calendar pb

--- controller/calendar.py
+++ controller/calendar.py
@@ -1,13 +1,8 @@
 import calendar
-from calendar import month
-import time
-import calendar
-import locale
 from datetime import datetime
 
-locale.setlocale(locale.LC_ALL, 'fr_FR.UTF-8')
-currentyear = datetime.now().year
-currentmonth = datetime.now().month
-
-print(calendar.month(currentyear, currentmonth))
-
+theyear = datetime.now().year
+themonth = datetime.now().month
+print(theyear)
+print(themonth)
+print(calendar.calendar(2020,2,1,6))
