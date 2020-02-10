1a503964531b7dc59871ee76cd84e0e29829b805
krid <baccarnh@gmail.com>
2020-02-10 17:04:38 +0100

    cal and main .py added in controller

--- controller/calendar.py
+++
@@ -1,8 +0,0 @@
-import calendar
-from datetime import datetime
-
-theyear = datetime.now().year
-themonth = datetime.now().month
-print(theyear)
-print(themonth)
-print(calendar.calendar(2020,2,1,6))
