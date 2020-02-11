1443fa9b2e4e447cedc184a8d76d068aba7853e4
krid <baccarnh@gmail.com>
2020-02-11 14:33:09 +0100

    controller created and main running but no hydratation using

--- controller/cal.py
+++ controller/cal.py
@@ -4,9 +4,29 @@ import time
 import calendar
 import locale
 from datetime import datetime
+def today():
+    locale.setlocale(locale.LC_ALL, 'fr_FR.UTF-8')
+    print(f"Nous sommes le : {datetime.today().strftime('%A %d %B %Y')}")
 
-locale.setlocale(locale.LC_ALL, 'fr_FR.UTF-8')
-currentyear = datetime.now().year
-currentmonth = datetime.now().month
 
-print(calendar.month(currentyear, currentmonth))
+def next_month():
+    currentyear = datetime.now().year
+    currentmonth = datetime.now().month
+    if currentmonth<12:
+        currentmonth+=1
+    else:
+        currentmonth=1
+        currentyear+=1
+    print(calendar.month(currentyear, currentmonth))
+
+def last_month():
+    currentyear = datetime.now().year
+    currentmonth = datetime.now().month
+    if currentmonth>1:
+        currentmonth-=1
+    else:
+        currentmonth=12
+        currentyear-=1
+    print(calendar.month(currentyear, currentmonth))
+
+
